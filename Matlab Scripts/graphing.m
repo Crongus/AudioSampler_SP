f7ba1f1c6fba46d975a5a8890f0850dbb6e70723
Crongus <l42liam@gmail.com>
2025-04-28 14:34:51 -0400

    adding the freq response csv

---
+++ Matlab Scripts/graphing.m
@@ -0,0 +1,4 @@
+nopitch = importdata('../CSVs/1khz_nopitch.csv')
+pluspitch = importdata('../CSVs/1khz_+pitch.csv')
+minuspitch = importdata('../CSVs/1khz_-pitch.csv')
+varnetwork = importdata('../CSVs/networkanalyzer1kPts')
